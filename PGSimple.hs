875acd112b85e5a9eb66afe8d9bf9bb74d65c0ed
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-01-20 22:02:22 +0500

    expand reexports

--- PGSimple.hs
+++ PGSimple.hs
@@ -5,19 +5,29 @@ module PGSimple
            -- * Some re-exports from postgresql-simple
        , Connection, connect, defaultConnectInfo, connectPostgreSQL
        , ToField(..), ToRow(..), FromField(..), FromRow(..)
-       , Query(..), Only(..), (:.)(..)
+       , Query(..), Only(..), In(..), Oid(..), Values(..)
+       , (:.)(..), PGArray(..) , HStoreList(..), HStoreMap(..)
+       , ToHStore(..), HStoreBuilder , hstore, parseHStoreList
+       , ToHStoreText(..), HStoreText , sqlQQ
        ) where
 
 
 import Database.PostgreSQL.Simple
-    ( Only(..), type (:.)(..), ToRow,
-      Connection, FromRow, defaultConnectInfo,
+    ( ToRow, Connection, FromRow, defaultConnectInfo,
       connectPostgreSQL, connect )
 import Database.PostgreSQL.Simple.FromField ( FromField(..) )
 import Database.PostgreSQL.Simple.FromRow ( FromRow(..) )
+import Database.PostgreSQL.Simple.HStore hiding
+    ( toBuilder, toLazyByteString ) -- to prevent conflicts
+import Database.PostgreSQL.Simple.SqlQQ
 import Database.PostgreSQL.Simple.ToField ( ToField(..) )
 import Database.PostgreSQL.Simple.ToRow ( ToRow(..) )
-import Database.PostgreSQL.Simple.Types ( Query(..) )
+import Database.PostgreSQL.Simple.Types
+import Language.Haskell.TH.Quote ( QuasiQuoter )
 
 import PGSimple.Types
 import PGSimple.Functions
+
+
+sqlQQ :: QuasiQuoter
+sqlQQ = sql
