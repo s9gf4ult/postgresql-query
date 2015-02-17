66a891f3d03d1f54b98dcae301723a554897e8a8
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-02-17 23:56:13 +0500

    types changed

--- src/PGSimple/Types.hs
+++ src/PGSimple/Types.hs
@@ -3,10 +3,7 @@ module PGSimple.Types
          InetText(..)
          -- * Connection pooling
        , HasPostgres(..)
-       , withPostgres
-       , withPostgresIO
          -- * Query execution monad
-       , PgMonad(..)
        , PgMonadT(..)
        , runPgMonadT
        , launchPG
@@ -29,8 +26,10 @@ import Control.Monad.Cont.Class ( MonadCont )
 import Control.Monad.Error.Class ( MonadError )
 import Control.Monad.IO.Class ( MonadIO(..) )
 import Control.Monad.Reader
-    ( MonadFix, MonadTrans, ReaderT(..), MonadReader(..) )
+    ( MonadFix, MonadTrans, ReaderT(..),
+      MonadReader(..), withReaderT )
 import Control.Monad.State.Class ( MonadState )
+import Control.Monad.Trans
 import Control.Monad.Trans.Control ( MonadBaseControl )
 import Control.Monad.Writer.Class ( MonadWriter )
 import Data.Int ( Int64 )
@@ -83,114 +82,33 @@ instance FromField InetText where
 
 
 class (MonadBase IO m) => HasPostgres m where
-    getPGpool :: m (Pool Connection)
-
-withPostgres :: (HasPostgres m, MonadBaseControl IO m)
-             => (Connection -> m a)
-             -> m a
-withPostgres action = do
-    pool <- getPGpool
-    withResource pool action
-
-
-withPostgresIO :: (HasPostgres m)
-               => (Connection -> IO a)
-               -> m a
-withPostgresIO action = do
-    pool <- getPGpool
-    liftBase
-        $ withResource pool action
-
-
--- | Typeclass for monad which can execute postgres queries
-class (Monad m) => PgMonad m where
-    mQuery               :: (ToRow q, FromRow r) => Query ->  q  -> m [r]
-    mQueryB              :: (FromRow r)          => SqlBuilder  -> m [r]
-    mQuery_              :: (FromRow r)          => Query       -> m [r]
-    mReturning           :: (ToRow q, FromRow r) => Query -> [q] -> m [r]
-    mExecute             :: (ToRow q)            => Query ->  q  -> m Int64
-    mExecuteB            ::                        SqlBuilder  -> m Int64
-    mExecute_            ::                        Query       -> m Int64
-    mExecuteMany         :: (ToRow q)            => Query -> [q] -> m Int64
-
-    -- | Run several queries in transaction. Commit transaction if everything
-    -- ok. Rollback if any exception catched.
-    -- Usage example:
-    --
-    -- @
-    -- handler :: Handler Id
-    -- handler = launchPG $ mWithTransaction defaultTransactionMode $ do
-    --     [aid] <- mExecute "INSERT INTO tbl(val) VALUES (?) RETURNING id" [10]
-    --     mExecuteMany "INSERT INTO tbl2(tbl_id, val) VALUES (?, ?)"
-    --         $ zipWith (,) (repeat aid) [1..10]
-    --     return aid
-    -- @
-    --
-    -- After executing we will have either exception thrown either new 11 rows
-    -- in database.
-    mWithTransactionMode :: TransactionMode -> m a -> m a
-
-    -- | The same as 'mWithTransactionMode' but use some default transaction
-    -- mode
-    mWithTransaction     :: m a -> m a
-    -- | Start transaction in some mode
-    mBeginMode           :: TransactionMode -> m ()
-    mBegin               :: m ()
-    mCommit              :: m ()
-    mRollback            :: m ()
-    -- FIXME: implement mWithSavepoint       :: m a -> m a
+    withPGConnection :: (Connection -> m a) -> m a
 
 
 newtype PgMonadT m a =
     PgMonadT
     { unPgMonadT :: ReaderT Connection m a
-    } deriving ( Functor, Applicative, Monad, MonadReader Connection
-               , MonadWriter w, MonadState s, MonadError e, MonadTrans
-               , Alternative, MonadFix, MonadPlus, MonadIO, MonadCont
-               , MonadThrow, MonadCatch, MonadMask, MonadBase b )
+    } deriving ( Functor, Applicative, Monad , MonadWriter w
+               , MonadState s, MonadError e, MonadTrans
+               , Alternative, MonadFix, MonadPlus, MonadIO
+               , MonadCont , MonadThrow, MonadCatch, MonadMask
+               , MonadBase b )
 
-instance (MonadIO m, MonadCatch m, MonadMask m)
-         => PgMonad (PgMonadT m) where
-    mQuery q ps = do
-        con <- ask
-        liftIO $ query con q ps
-    mQueryB bld = do
-        con <- ask
-        liftIO $ do
-            b <- sqlBuild bld con
-            query_ con $ Query $ toByteString b
-    mQuery_ q = do
-        con <- ask
-        liftIO $ query_ con q
-    mReturning q ps = do
-        con <- ask
-        liftIO $ returning con q ps
-    mExecute q ps = do
-        con <- ask
-        liftIO $ execute con q ps
-    mExecuteB bld = do
-        con <- ask
-        liftIO $ do
-            b <- sqlBuild bld con
-            execute_ con $ Query $ toByteString b
-    mExecute_ q = do
-        con <- ask
-        liftIO $ execute_ con q
-    mExecuteMany q ps = do
-        con <- ask
-        liftIO $ executeMany con q ps
+instance (MonadReader r m) => MonadReader r (PgMonadT m) where
+    ask = lift ask
+    local md ac = do
+        con <- PgMonadT ask
+        lift $ do
+            r <- ask
+            local md $ runPgMonadT con ac
+    reader = lift . reader
 
-    mWithTransactionMode mode act = do --  NOTE: copy-pasted
-          mask $ \restore -> do
-              mBeginMode mode
-              r <- restore act `onException` mRollback
-              mCommit
-              return r
-    mWithTransaction act = mWithTransactionMode defaultTransactionMode act
-    mBeginMode mode = ask >>= liftIO . beginMode mode
-    mBegin = ask >>= liftIO . begin
-    mCommit = ask >>= liftIO . commit
-    mRollback = ask >>= liftIO . rollback
+
+instance (MonadBase IO m) => HasPostgres (PgMonadT m) where
+    withPGConnection action = do
+        con <- PgMonadT ask
+        action con
+
 
 
 runPgMonadT :: Connection -> PgMonadT m a -> m a
@@ -206,11 +124,11 @@ runPgMonadT con (PgMonadT action) = runReaderT action con
 --     a <- mQuery_ "SELECT val FROM tbl"
 --     return a
 -- @
-launchPG :: (HasPostgres m, MonadBaseControl IO m)
+launchPG :: (HasPostgres m)
          => PgMonadT m a
          -> m a
-launchPG act = withPostgres
-               $ \con -> runPgMonadT con act
+launchPG act = withPGConnection $ \con -> do
+    runPgMonadT con act
 
 
 -- | Auxiliary typeclass for data types which can map to rows of some
