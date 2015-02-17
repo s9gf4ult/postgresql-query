ce351ffa01fd0b58c02b633f5d66277603ac530e
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-02-17 13:28:36 +0500

    `IsString` isntance for `SqlBuilder`

--- src/PGSimple/SqlBuilder.hs
+++ src/PGSimple/SqlBuilder.hs
@@ -3,19 +3,18 @@ module PGSimple.SqlBuilder where
 import Prelude
 
 import Blaze.ByteString.Builder ( Builder )
-import Blaze.ByteString.Builder.ByteString
-    ( fromByteString, fromLazyByteString )
-import Blaze.ByteString.Builder.Char.Utf8
-    ( fromString, fromText, fromLazyText )
 import Control.Applicative
 import Control.Exception
 import Data.ByteString ( ByteString )
 import Data.Monoid
+import Data.String
 import Data.Typeable ( Typeable )
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.ToField
 import GHC.Generics ( Generic )
 
+import qualified Blaze.ByteString.Builder.ByteString as BB
+import qualified Blaze.ByteString.Builder.Char.Utf8 as BB
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
@@ -26,6 +25,11 @@ newtype SqlBuilder =
     { sqlBuild :: Connection -> IO Builder }
     deriving (Typeable, Generic)
 
+instance IsString SqlBuilder where
+    fromString s =
+        let b = fromString s :: ByteString
+        in toSqlBuilder b
+
 class ToSqlBuilder a where
     toSqlBuilder :: a -> SqlBuilder
 
@@ -34,18 +38,18 @@ instance ToSqlBuilder SqlBuilder where
 instance ToSqlBuilder Builder where
     toSqlBuilder = sqlBuilderPure
 instance ToSqlBuilder ByteString where
-    toSqlBuilder = sqlBuilderPure . fromByteString
+    toSqlBuilder = sqlBuilderPure . BB.fromByteString
 instance ToSqlBuilder BL.ByteString where
-    toSqlBuilder = sqlBuilderPure . fromLazyByteString
+    toSqlBuilder = sqlBuilderPure . BB.fromLazyByteString
 instance ToSqlBuilder String where
-    toSqlBuilder = sqlBuilderPure . fromString
+    toSqlBuilder = sqlBuilderPure . BB.fromString
 instance ToSqlBuilder T.Text where
-    toSqlBuilder = sqlBuilderPure . fromText
+    toSqlBuilder = sqlBuilderPure . BB.fromText
 instance ToSqlBuilder TL.Text where
-    toSqlBuilder = sqlBuilderPure . fromLazyText
+    toSqlBuilder = sqlBuilderPure . BB.fromLazyText
 instance ToRow row => ToSqlBuilder (Query, row) where
     toSqlBuilder (q, row) = SqlBuilder $ \c ->
-        fromByteString <$> formatQuery c q row
+        BB.fromByteString <$> formatQuery c q row
 
 
 
@@ -74,13 +78,13 @@ throwFormatError q msg = throw
 
 
 quoteOrThrow :: Query -> Either ByteString ByteString -> Builder
-quoteOrThrow q = either (throwFormatError q) (inQuotes . fromByteString)
+quoteOrThrow q = either (throwFormatError q) (inQuotes . BB.fromByteString)
 
 -- | Shity copy-paste from postgresql-simple
 buildAction :: Connection -> Query -> Action -> IO Builder
 buildAction _ _ (Plain b)            = pure b
 buildAction c q (Escape s)           = quoteOrThrow q <$> escapeStringConn c s
 buildAction c q (EscapeByteA s)      = quoteOrThrow q <$> escapeByteaConn c s
-buildAction c q (EscapeIdentifier s) = either (throwFormatError q) fromByteString
+buildAction c q (EscapeIdentifier s) = either (throwFormatError q) BB.fromByteString
                                      <$> escapeIdentifier c s
 buildAction c q (Many  ys)           = mconcat <$> mapM (buildAction c q) ys
