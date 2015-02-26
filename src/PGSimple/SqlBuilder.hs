5b728877a93179a8e8180c88db0eb1be7b3f7672
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-02-26 15:22:47 +0500

    fix for patched pgs

--- src/PGSimple/SqlBuilder.hs
+++ src/PGSimple/SqlBuilder.hs
@@ -17,13 +17,14 @@ import Prelude
 import Blaze.ByteString.Builder
     ( Builder, toByteString )
 import Control.Applicative
-import Control.Exception
 import Data.ByteString ( ByteString )
 import Data.Monoid
 import Data.String
 import Data.Text ( Text )
 import Data.Typeable ( Typeable )
 import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.Internal
+    ( buildAction )
 import Database.PostgreSQL.Simple.ToField
 import Database.PostgreSQL.Simple.Types
     ( Query(..), Identifier(..) )
@@ -33,7 +34,6 @@ import qualified Blaze.ByteString.Builder.ByteString as BB
 import qualified Blaze.ByteString.Builder.Char.Utf8 as BB
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy as TL
 
 {- $setup
@@ -124,32 +124,9 @@ sqlBuilderPure b = SqlBuilder $ const $ pure b
 
 sqlBuilderFromField :: (ToField a) => Query -> a -> SqlBuilder
 sqlBuilderFromField q a =
-    SqlBuilder $ \c -> buildAction c q $ toField a
+    SqlBuilder $ \c -> buildAction c q [] $ toField a --  FIXME: pass something as parameters string
 
 instance Monoid SqlBuilder where
     mempty = sqlBuilderPure mempty
     mappend (SqlBuilder a) (SqlBuilder b) =
         SqlBuilder $ \c -> mappend <$> (a c) <*> (b c)
-
-throwFormatError :: Query -> ByteString -> a
-throwFormatError q msg = throw
-             $ FormatError
-               { fmtMessage = utf8ToString msg
-               , fmtQuery = q
-               , fmtParams = [] --  FIXME: Maybe paste something here
-               }
-  where
-    utf8ToString = T.unpack . T.decodeUtf8
-
--- | for internal usage
-quoteOrThrow :: Query -> Either ByteString ByteString -> Builder
-quoteOrThrow q = either (throwFormatError q) (inQuotes . BB.fromByteString)
-
--- | Shity copy-paste from postgresql-simple
-buildAction :: Connection -> Query -> Action -> IO Builder
-buildAction _ _ (Plain b)            = pure b
-buildAction c q (Escape s)           = quoteOrThrow q <$> escapeStringConn c s
-buildAction c q (EscapeByteA s)      = quoteOrThrow q <$> escapeByteaConn c s
-buildAction c q (EscapeIdentifier s) = either (throwFormatError q) BB.fromByteString
-                                     <$> escapeIdentifier c s
-buildAction c q (Many  ys)           = mconcat <$> mapM (buildAction c q) ys
