f4899adb1ad23917468422f6e9b01b019531233d
Aleksey Uimanov <s9gf4ult@gmail.com>
2016-05-05 13:11:44 +0500

    making typelevel transactions

---
+++ src/Database/PostgreSQL/Query/Entity/Class.hs
@@ -0,0 +1,26 @@
+module Database.PostgreSQL.Query.Entity.Class
+       ( Entity(..)
+       , Ent
+       ) where
+
+import Data.Proxy
+import Data.Text ( Text )
+import Data.Typeable ( Typeable )
+import Database.PostgreSQL.Query.Types
+
+-- | Auxiliary typeclass for data types which can map to rows of some
+-- table. This typeclass is used inside functions like 'pgSelectEntities' to
+-- generate queries.
+class Entity a where
+    -- | Id type for this entity
+    data EntityId a :: *
+    -- | Table name of this entity
+    tableName :: Proxy a -> FN
+    -- | Field names without 'id' and 'created'. The order of field names must match
+    -- with order of fields in 'ToRow' and 'FromRow' instances of this type.
+    fieldNames :: Proxy a -> [FN]
+
+deriving instance Typeable EntityId
+
+-- | Entity with it's id
+type Ent a = (EntityId a, a)
--- src/Database/PostgreSQL/Query/Types.hs
+++ src/Database/PostgreSQL/Query/Types.hs
@@ -1,10 +1,15 @@
 module Database.PostgreSQL.Query.Types
        ( -- * Query execution
-         HasPostgres(..)
-       , TransactionSafe
-       , PgMonadT(..)
-       , runPgMonadT
-       , launchPG
+         MonadPostgresPool(..)
+       -- , TransactionSafe
+       -- , PgMonadT(..)
+       -- , runPgMonadT
+       -- , launchPG
+       , MonadTransaction(..)
+       , MonadPostgres(..)
+       , PostgresT(..)
+       , runPostgresT
+       , runPG
         -- * Auxiliary types
        , InetText(..)
        , FN(..)
@@ -14,47 +19,49 @@ module Database.PostgreSQL.Query.Types
        , ToMarkedRow(..)
        ) where
 
-import Prelude
+import           Prelude
 
-import Control.Applicative
-import Control.Monad
-import Control.Monad.Base ( MonadBase(..) )
-import Control.Monad.Catch
+import           TypeFun.Data.Peano
+import           Control.Applicative
+import           Control.Monad
+import           Control.Monad.Base ( MonadBase(..) )
+import           Control.Monad.Catch
     ( MonadThrow, MonadMask, MonadCatch )
-import Control.Monad.Cont.Class ( MonadCont )
-import Control.Monad.Error.Class ( MonadError )
-import Control.Monad.Fix ( MonadFix(..) )
-import Control.Monad.HReader
-import Control.Monad.Logger
-import Control.Monad.Reader
+import           Control.Monad.Cont.Class ( MonadCont )
+import           Control.Monad.Error.Class ( MonadError )
+import           Control.Monad.Fix ( MonadFix(..) )
+import           Control.Monad.HReader
+import           Control.Monad.Logger
+import           Control.Monad.Reader
     ( MonadReader(..), ReaderT(..) )
-import Control.Monad.State.Class ( MonadState )
-import Control.Monad.Trans
-import Control.Monad.Trans.Cont
-import Control.Monad.Trans.Control
-import Control.Monad.Trans.Either
-import Control.Monad.Trans.Except
-import Control.Monad.Trans.Identity
-import Control.Monad.Trans.Maybe
-import Control.Monad.Writer.Class ( MonadWriter )
-import Data.HSet
-import Data.Monoid
-import Data.Pool
-import Data.String
-import Data.Text ( Text )
-import Data.Typeable
-import Database.PostgreSQL.Query.SqlBuilder
+import           Control.Monad.State.Class ( MonadState )
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Cont
+import           Control.Monad.Trans.Control
+import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Identity
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.Writer.Class ( MonadWriter )
+import           Data.HSet
+import           Data.Monoid
+import           Data.Pool
+import           Data.String
+import           Data.Text ( Text )
+import           Data.Typeable
+import           Database.PostgreSQL.Query.SqlBuilder
     ( mkIdent, ToSqlBuilder(..), SqlBuilder(..) )
-import Database.PostgreSQL.Query.TH.SqlExp
+import           Database.PostgreSQL.Query.TH.SqlExp
     ( sqlExp )
-import Database.PostgreSQL.Simple
-import Database.PostgreSQL.Simple.FromField
+import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.FromField
     ( FromField(..), typename, returnError )
-import Database.PostgreSQL.Simple.ToField
+import           Database.PostgreSQL.Simple.ToField
     ( ToField )
-import GHC.Generics
-import Instances.TH.Lift ()
-import Language.Haskell.TH.Lift ( deriveLift )
+import           GHC.Generics
+import           GHC.TypeLits
+import           Instances.TH.Lift ()
+import           Language.Haskell.TH.Lift ( deriveLift )
 
 import qualified Data.List as L
 import qualified Control.Monad.Trans.State.Lazy as STL
@@ -205,181 +212,190 @@ mrToBuilder b (MR l) = mconcat
 -- computation. It can be reader of pool of connections or just reader of
 -- connection
 
-class (MonadBase IO m) => HasPostgres m where
+class (MonadBase IO m) => MonadPostgresPool m where
     withPGConnection :: (Connection -> m a) -> m a
 
-instance (HasPostgres m) => HasPostgres (EitherT e m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (EitherT e m) where
     withPGConnection action = do
         EitherT $ withPGConnection $ \con -> do
             runEitherT $ action con
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (ExceptT e m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (ExceptT e m) where
     withPGConnection action = do
         ExceptT $ withPGConnection $ \con -> do
             runExceptT $ action con
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (IdentityT m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (IdentityT m) where
     withPGConnection action = do
         IdentityT $ withPGConnection $ \con -> do
             runIdentityT $ action con
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (MaybeT m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (MaybeT m) where
     withPGConnection action = do
         MaybeT $ withPGConnection $ \con -> do
             runMaybeT $ action con
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (ReaderT r m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (ReaderT r m) where
     withPGConnection action = do
         ReaderT $ \r -> withPGConnection $ \con ->
             runReaderT (action con) r
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (STL.StateT s m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (STL.StateT s m) where
     withPGConnection action = do
         STL.StateT $ \s -> withPGConnection $ \con ->
             STL.runStateT (action con) s
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (STS.StateT s m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (STS.StateT s m) where
     withPGConnection action = do
         STS.StateT $ \s -> withPGConnection $ \con ->
             STS.runStateT (action con) s
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m) => HasPostgres (ContT r m) where
+instance (MonadPostgresPool m) => MonadPostgresPool (ContT r m) where
     withPGConnection action = do
         ContT $ \r -> withPGConnection $ \con ->
             runContT (action con) r
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m, Monoid w) => HasPostgres (WL.WriterT w m) where
+instance (MonadPostgresPool m, Monoid w) => MonadPostgresPool (WL.WriterT w m) where
     withPGConnection action = do
         WL.WriterT $ withPGConnection $ \con ->
             WL.runWriterT (action con)
     {-# INLINABLE withPGConnection #-}
 
-instance (HasPostgres m, Monoid w) => HasPostgres (WS.WriterT w m) where
+instance (MonadPostgresPool m, Monoid w) => MonadPostgresPool (WS.WriterT w m) where
     withPGConnection action = do
         WS.WriterT $ withPGConnection $ \con ->
             WS.runWriterT (action con)
     {-# INLINABLE withPGConnection #-}
 
 instance (MonadBase IO m, MonadBaseControl IO m, HGettable els (Pool Connection))
-         => HasPostgres (HReaderT els m) where
+         => MonadPostgresPool (HReaderT els m) where
     withPGConnection action = do
         pool <- hask
         withResource pool action
 
--- | Empty typeclass signing monad in which transaction is
--- safe. i.e. `PgMonadT` have this instance, but some other monad giving
--- connection from e.g. connection pool is not.
-class TransactionSafe (m :: * -> *)
-
-instance (TransactionSafe m) => TransactionSafe (EitherT e m)
-instance (TransactionSafe m) => TransactionSafe (ExceptT e m)
-instance (TransactionSafe m) => TransactionSafe (IdentityT m)
-instance (TransactionSafe m) => TransactionSafe (MaybeT m)
-instance (TransactionSafe m) => TransactionSafe (ReaderT r m)
-instance (TransactionSafe m) => TransactionSafe (STL.StateT s m)
-instance (TransactionSafe m) => TransactionSafe (STS.StateT s m)
-instance (TransactionSafe m) => TransactionSafe (ContT r m)
-instance (TransactionSafe m, Monoid w) => TransactionSafe (WL.WriterT w m)
-instance (TransactionSafe m, Monoid w) => TransactionSafe (WS.WriterT w m)
-
-
--- | Reader of connection. Has instance of 'HasPostgres'. So if you have a
--- connection you can run queries in this monad using 'runPgMonadT'. Or you
--- can use this transformer to run sequence of queries using same
--- connection with 'launchPG'.
-newtype PgMonadT m a = PgMonadT
-    { unPgMonadT :: ReaderT Connection m a
+-- | Reader of connection. If you have a
+-- connection you can run queries in this monad using 'runPgMonadT'. Or you can
+-- use this transformer to run sequence of queries using same connection with
+-- 'launchPG'.
+newtype PostgresT (level :: N) m a = PostgresT
+    { unPostgresT :: ReaderT Connection m a
     } deriving ( Functor, Applicative, Monad , MonadWriter w
                , MonadState s, MonadError e, MonadTrans
                , Alternative, MonadFix, MonadPlus, MonadIO
                , MonadCont, MonadThrow, MonadCatch, MonadMask
                , MonadBase b, MonadLogger )
 
-#if MIN_VERSION_monad_control(1,0,0)
-instance (MonadBaseControl b m) => MonadBaseControl b (PgMonadT m) where
-    type StM (PgMonadT m) a = StM (ReaderT Connection m) a
-    liftBaseWith action = PgMonadT $ do
-        liftBaseWith $ \runInBase -> action (runInBase . unPgMonadT)
-    restoreM st = PgMonadT $ restoreM st
-    {-# INLINABLE liftBaseWith #-}
-    {-# INLINABLE restoreM #-}
+class (MonadBase IO m) => MonadPostgres m where
+  type TransactionLevel m :: N
+  askConnection :: m Connection
 
-instance MonadTransControl PgMonadT where
-    type StT PgMonadT a = StT (ReaderT Connection) a
-    liftWith action = PgMonadT $ do
-        liftWith $ \runTrans -> action (runTrans . unPgMonadT)
-    restoreT st = PgMonadT $ restoreT st
-    {-# INLINABLE liftWith #-}
-    {-# INLINABLE restoreT #-}
-#else
-instance (MonadBaseControl b m) => MonadBaseControl b (PgMonadT m) where
-    newtype StM (PgMonadT m) a
-        = PgMTM (StM (ReaderT Connection m) a)
-    liftBaseWith action = PgMonadT $ do
-        liftBaseWith $ \runInBase -> do
-            action ((PgMTM `liftM`) . runInBase . unPgMonadT)
-    restoreM (PgMTM st) = PgMonadT $ restoreM st
-    {-# INLINABLE liftBaseWith #-}
-    {-# INLINABLE restoreM #-}
+instance (MonadBase IO m) => MonadPostgres (PostgresT level m) where
+  type TransactionLevel (PostgresT level m) = level
+  askConnection = PostgresT ask
 
-instance MonadTransControl PgMonadT where
-    newtype StT PgMonadT a
-        = PgMTT
-          { unPgMTT :: StT (ReaderT Connection) a
-          }
-    liftWith action = PgMonadT $ do
-        liftWith $ \runTrans -> do -- ReaderT Connection n a -> n (StT (ReaderT Connection n) a)
-            action ((PgMTT `liftM`) . runTrans . unPgMonadT)
-    restoreT st = PgMonadT $ restoreT $ unPgMTT `liftM` st
-    {-# INLINABLE liftWith #-}
-    {-# INLINABLE restoreT #-}
-#endif
+instance (MonadPostgres m) => MonadPostgres (ReaderT r m) where
+  type TransactionLevel (ReaderT r m) = TransactionLevel m
+  askConnection = lift askConnection
 
-instance (MonadReader r m) => MonadReader r (PgMonadT m) where
-    ask = lift ask
-    local md ac = do
-        con <- PgMonadT ask
-        lift $ do
-            local md $ runPgMonadT con ac
-    reader = lift . reader
-    {-# INLINABLE ask #-}
-    {-# INLINABLE local #-}
-    {-# INLINABLE reader #-}
+class ( MonadPostgres m, MonadPostgres n
+      , ('S (TransactionLevel m)) ~ TransactionLevel n
+      ) => MonadTransaction n m | n -> m, m -> n where
+  liftLevel :: n a -> m a
 
-instance (MonadHReader m) => MonadHReader (PgMonadT m) where
-  type MHRElements (PgMonadT m) = MHRElements m
-  askHSet = PgMonadT askHSet
-  {-# INLINEABLE askHSet #-}
+instance ( MonadBase IO m
+         ) => MonadTransaction (PostgresT ('S level) m) (PostgresT level m) where
+  liftLevel (PostgresT a) = PostgresT a
 
-instance (MonadBase IO m) => HasPostgres (PgMonadT m) where
-    withPGConnection action = do
-        con <- PgMonadT ask
-        action con
-    {-# INLINABLE withPGConnection #-}
+instance ( MonadTransaction n m
+         ) => MonadTransaction (ReaderT r n) (ReaderT r m) where
+  liftLevel (ReaderT na) = ReaderT $ liftLevel . na
 
-instance TransactionSafe (PgMonadT m)
+-- #if MIN_VERSION_monad_control(1,0,0)
+-- instance (MonadBaseControl b m) => MonadBaseControl b (PgMonadT m) where
+--     type StM (PgMonadT m) a = StM (ReaderT Connection m) a
+--     liftBaseWith action = PgMonadT $ do
+--         liftBaseWith $ \runInBase -> action (runInBase . unPgMonadT)
+--     restoreM st = PgMonadT $ restoreM st
+--     {-# INLINABLE liftBaseWith #-}
+--     {-# INLINABLE restoreM #-}
+
+-- instance MonadTransControl PgMonadT where
+--     type StT PgMonadT a = StT (ReaderT Connection) a
+--     liftWith action = PgMonadT $ do
+--         liftWith $ \runTrans -> action (runTrans . unPgMonadT)
+--     restoreT st = PgMonadT $ restoreT st
+--     {-# INLINABLE liftWith #-}
+--     {-# INLINABLE restoreT #-}
+-- #else
+-- instance (MonadBaseControl b m) => MonadBaseControl b (PgMonadT m) where
+--     newtype StM (PgMonadT m) a
+--         = PgMTM (StM (ReaderT Connection m) a)
+--     liftBaseWith action = PgMonadT $ do
+--         liftBaseWith $ \runInBase -> do
+--             action ((PgMTM `liftM`) . runInBase . unPgMonadT)
+--     restoreM (PgMTM st) = PgMonadT $ restoreM st
+--     {-# INLINABLE liftBaseWith #-}
+--     {-# INLINABLE restoreM #-}
+
+-- instance MonadTransControl PgMonadT where
+--     newtype StT PgMonadT a
+--         = PgMTT
+--           { unPgMTT :: StT (ReaderT Connection) a
+--           }
+--     liftWith action = PgMonadT $ do
+--         liftWith $ \runTrans -> do -- ReaderT Connection n a -> n (StT (ReaderT Connection n) a)
+--             action ((PgMTT `liftM`) . runTrans . unPgMonadT)
+--     restoreT st = PgMonadT $ restoreT $ unPgMTT `liftM` st
+--     {-# INLINABLE liftWith #-}
+--     {-# INLINABLE restoreT #-}
+-- #endif
+
+-- instance (MonadReader r m) => MonadReader r (PgMonadT m) where
+--     ask = lift ask
+--     local md ac = do
+--         con <- PgMonadT ask
+--         lift $ do
+--             local md $ runPgMonadT con ac
+--     reader = lift . reader
+--     {-# INLINABLE ask #-}
+--     {-# INLINABLE local #-}
+--     {-# INLINABLE reader #-}
+
+-- instance (MonadHReader m) => MonadHReader (PgMonadT m) where
+--   type MHRElements (PgMonadT m) = MHRElements m
+--   askHSet = PgMonadT askHSet
+--   {-# INLINEABLE askHSet #-}
+
+-- instance (MonadBase IO m) => HasPostgres (PgMonadT m) where
+--     withPGConnection action = do
+--         con <- PgMonadT ask
+--         action con
+--     {-# INLINABLE withPGConnection #-}
+
+-- instance TransactionSafe (PgMonadT m)
 
 
-runPgMonadT :: Connection -> PgMonadT m a -> m a
-runPgMonadT con (PgMonadT action) = runReaderT action con
+runPostgresT :: Connection -> PostgresT 'Z m a -> m a
+runPostgresT con (PostgresT action) = runReaderT action con
 
-{- | If your monad have instance of 'HasPostgres' you maybe dont need this
+{- | If your monad have instance of 'MonadPostgresPool' you maybe dont need this
 function, unless your instance use 'withPGPool' which acquires connection
 from pool for each query. If you want to run sequence of queries using same
 connection you need this function
 
 -}
 
-launchPG :: (HasPostgres m)
-         => PgMonadT m a
-         -> m a
-launchPG act = withPGConnection $ \con -> do
-    runPgMonadT con act
+runPG
+  :: (MonadPostgresPool m)
+  => PostgresT 'Z m a
+  -> m a
+runPG act = withPGConnection $ \con -> do
+    runPostgresT con act
