3223926aba5b6a8daff2c3e09af4bd9a5890a80e
Aleksey Uimanov <s9gf4ult@gmail.com>
2017-10-02 22:18:55 +0500

    Upd: delete old ghc-8 snapshot

--- testall.sh
+++ testall.sh
@@ -4,7 +4,5 @@ cp lts-2.9.yaml stack.yaml &&
 stack clean && stack test &&
 cp lts-6.7.yaml stack.yaml &&
 stack clean && stack test &&
-cp ghc-8.0.1.yaml stack.yaml &&
-stack clean && stack test
 cp ghc-8.2.1.yaml stack.yaml &&
 stack clean && stack test
